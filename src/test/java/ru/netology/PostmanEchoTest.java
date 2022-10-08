de53a6c38a9ab20e6d8a17ffde03f84ddb559269
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-10-08 16:35:38 +0300

    testPassing

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("ANY data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
